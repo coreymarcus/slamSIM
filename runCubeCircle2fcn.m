a0bf87a399b35b077a0ddb1893a5a6f2a64fa4d8
CoreyOnAtlantis <corey.marcus@gmail.com>
2020-11-20 12:54:48 -0600

    config changes

--- runCubeCircle2fcn.m
+++ runCubeCircle2fcn.m
@@ -23,7 +23,7 @@ Noscil = 70; %number of oscillations per revolution
 N = round(Revs*Nrev);
 
 %control if we used compiled code for image generation
-useMexForImgGen = true;
+useMexForImgGen = false;
 
 %target KF, zero index
 targKF = 1;
@@ -101,21 +101,28 @@ C10 = createCube(P10, s, sw, fc, ec);
 P11 = [.2; -1; 2];
 C11 = createCube(P11, s, sw, fc, ec);
 
-%cube cell array
+%cube cell array rich environment
+% CArray = cell(1);
+% CArray{1,1} = C;
+% CArray{2,1} = C2;
+% CArray{3,1} = C3;
+% CArray{4,1} = C4;
+% CArray{5,1} = C5;
+% CArray{6,1} = C6;
+% CArray{7,1} = C7;
+% CArray{8,1} = C8;
+% CArray{9,1} = C9;
+% CArray{10,1} = C10;
+% CArray{11,1} = C11;
+
+%cube cell array sparse environment
 CArray = cell(1);
 CArray{1,1} = C;
 CArray{2,1} = C2;
 CArray{3,1} = C3;
 CArray{4,1} = C4;
-CArray{5,1} = C5;
-CArray{6,1} = C6;
-CArray{7,1} = C7;
-CArray{8,1} = C8;
-CArray{9,1} = C9;
-CArray{10,1} = C10;
-CArray{11,1} = C11;
 
-%cube cell array
+%cube cell array without encompassing cube
 % CArray = cell(1);
 % CArray{1} = C;
 % CArray{2} = C2;
